cf234242f761992f6fc8a868b9a86685715be60e
Sander Østrem Fagernes <sanderof@stud.ntnu.no>
2023-03-13 08:24:32 +0100

    14 Main menu screen setup

---
+++ frontend/core/src/com/game/tankwars/controller/MainMenuController.java
@@ -0,0 +1,63 @@
+package com.game.tankwars.controller;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.game.tankwars.TankWarsGame;
+import com.game.tankwars.model.MenuButton;
+import com.game.tankwars.view.GameScreen;
+
+public class MainMenuController {
+
+    private final TankWarsGame tankWarsGame;
+    private String username;
+    private final Array<MenuButton> menuButtons;
+
+
+    public MainMenuController(final TankWarsGame tankWarsGame, final BitmapFont font) {
+        this.tankWarsGame = tankWarsGame;
+
+        fetchUser();
+
+        this.menuButtons = new Array<>();
+    }
+
+    public Array<MenuButton> setMenuButtons(Texture buttonTexture, BitmapFont font) {
+        this.menuButtons.add(new MenuButton(buttonTexture, font, "Find Game"));
+        this.menuButtons.add(new MenuButton(buttonTexture, font,  "Leaderboard"));
+        this.menuButtons.add(new MenuButton(buttonTexture, font, "Settings"));
+        this.menuButtons.add(new MenuButton(buttonTexture, font,  "Log out"));
+
+        return menuButtons;
+    }
+
+
+    public void handleInput(Vector3 touchPos) {
+
+        for (int i = 0; i < menuButtons.size; i++) {
+            MenuButton menuButton = menuButtons.get(i);
+            if (touchPos.x >= menuButton.getX() && touchPos.x <= menuButton.getX() + menuButton.getWidth() &&
+                touchPos.y >= menuButton.getY() && touchPos.y <= menuButton.getY() + menuButton.getHeight()) {
+
+                switch (i) {
+                    case 0: tankWarsGame.setScreen(new GameScreen(tankWarsGame)); break;
+                    case 1: System.out.println("Leaderboard button: Not yet functional"); break;
+                    case 2: System.out.println("Settings button: Not yet functional"); break;
+                    case 3: System.out.println("Log out button: Not yet functional"); break;
+                }
+            }
+        }
+    }
+
+    /**
+     * TODO: Fetch user from backend. For now it only creates a dummy username to pass to the screen
+     */
+    private void fetchUser() {
+        this.username = "Commander";
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
--- frontend/core/src/com/game/tankwars/view/MainMenuScreen.java
+++ frontend/core/src/com/game/tankwars/view/MainMenuScreen.java
@@ -1,35 +1,119 @@
 package com.game.tankwars.view;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import com.game.tankwars.TankWarsGame;
+import com.game.tankwars.controller.MainMenuController;
+import com.game.tankwars.model.MenuButton;
 
-public class MainMenuScreen implements Screen {
-    final TankWarsGame tankWarsGame;
-    OrthographicCamera camera;
+public class MainMenuScreen extends InputAdapter implements Screen {
 
-    public MainMenuScreen(final TankWarsGame tankWarsGame){
+    private final TankWarsGame tankWarsGame;
+    private final SpriteBatch batch;
+    private final BitmapFont font;
+    private final Viewport viewport;
+    private final MainMenuController controller;
+
+    private final Sprite logo;
+    private final Texture welcomeBox;
+    private final Texture background;
+    private final Texture menuButtonTexture;
+    private final GlyphLayout welcomeLayout, usernameLayout;
+
+    private final Array<MenuButton> menuButtons;
+
+    private final Vector3 touchPos;
+
+    private float heightPercentile;
+
+    /**
+     * Main menu
+     * Only supports portrait mode
+     */
+    public MainMenuScreen(final TankWarsGame tankWarsGame, final SpriteBatch batch, final BitmapFont font, final OrthographicCamera camera) {
         this.tankWarsGame = tankWarsGame;
-        camera = new OrthographicCamera();
+        this.batch = batch;
+        this.font = font;
+        this.font.getData().setScale(0.55f);
 
-        //NB! Temporarily directing to GameScreen, this is subject to change
-        GameScreen gameScreen = new GameScreen(this.tankWarsGame);
-        tankWarsGame.setScreen(gameScreen);
-    }
+        // TODO: Add landscape support, both on startup and on rotation
+        viewport = new FitViewport(camera.viewportWidth, camera.viewportHeight, camera);
+        heightPercentile = viewport.getWorldHeight() / 100;
+        controller = new MainMenuController(this.tankWarsGame, this.font);
 
-    @Override
-    public void render(float delta) {
+        logo = new Sprite(new Texture("tankwars-logo.png"));
+        logo.setScale(1.3f);
+        logo.setPosition(-logo.getWidth() / 2, 32 * heightPercentile);
 
+        welcomeBox = new Texture("main-menu-welcome-box.png");
+        welcomeLayout = new GlyphLayout(font, "Welcome to duty");
+        usernameLayout = new GlyphLayout(font, controller.getUsername());
+
+        background = new Texture("menu-background.png");
+
+        menuButtonTexture = new Texture("menu-button-2.png");
+        menuButtons = controller.setMenuButtons(menuButtonTexture, font);
+        for (int i = 0; i < menuButtons.size; i++) {
+            menuButtons.get(i).setPosition(0,  -(12 * i - 4) * heightPercentile);
+        }
+
+        touchPos = new Vector3();
     }
 
     @Override
     public void show() {
+        Gdx.input.setInputProcessor(this);
+    }
 
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0, 1);
+        batch.setProjectionMatrix(viewport.getCamera().combined);
+
+        batch.begin();
+        batch.draw(background, -background.getWidth() / 2f, -background.getHeight() / 2f);
+
+        logo.draw(batch);
+
+        batch.draw(welcomeBox, -welcomeBox.getWidth() / 2f, 12 * heightPercentile,
+                viewport.getWorldWidth(), welcomeBox.getHeight());
+        font.draw(batch, welcomeLayout, -welcomeLayout.width / 2,
+                12 * heightPercentile + welcomeBox.getHeight() * 4f/5f);
+        font.draw(batch, usernameLayout, -usernameLayout.width / 2,
+                12 * heightPercentile + welcomeBox.getHeight() * 4f/5f - 2 * welcomeLayout.height);
+
+        for (MenuButton menuButton : menuButtons) {
+            batch.draw(menuButton.getTexture(), menuButton.getX(), menuButton.getY());
+            font.draw(batch, menuButton.getContent(), menuButton.getContentX(), menuButton.getContentY());
+        }
+        batch.end();
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        touchPos.set(screenX, screenY, 0);
+        viewport.unproject(touchPos);
+
+        controller.handleInput(touchPos);
+        return true;
     }
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height);
+        heightPercentile = viewport.getWorldHeight() / 100;
     }
 
     @Override
@@ -44,11 +128,14 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void hide() {
-
+        Gdx.input.setInputProcessor(null);
     }
 
     @Override
     public void dispose() {
-
+        logo.getTexture().dispose();
+        welcomeBox.dispose();
+        background.dispose();
+        menuButtonTexture.dispose();
     }
 }
