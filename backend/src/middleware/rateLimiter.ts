9690dd760b40196f749f2f4c08de30d9f9ec7092
Fredrik Fonn Hansen <fredrfha@stud.ntnu.no>
2023-04-15 14:15:41 +0200

    Backend 30 req

--- backend/src/middleware/rateLimiter.ts
+++ backend/src/middleware/rateLimiter.ts
@@ -3,7 +3,7 @@ const rateLimit = require('express-rate-limit');
 // Configure rate limiter
 export const rateLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute in milliseconds
-  max: 15, // Limit each IP to 1000 requests per windowMs
+  max: 30, // Limit each IP to 1000 requests per windowMs
   statusCode: 429, // HTTP status code to send when rate limit is exceeded (Too Many Requests)
   message: 'Too many requests, please try again later.',
 });
